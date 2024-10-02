ccac42aea53f213dbb05ba6fef5fc66a022edcd4
Igor <dumanschi.igor@gmail.com>
2024-10-02 13:55:18 +0300

    JS_1_HW

---
+++ main.js
@@ -0,0 +1,84 @@
+// const userName = "Igor";
+// const userAge = 34;
+// const learnJS = true;
+// const jackpot = undefined;
+// const nul = null;
+// const symb = Symbol;
+// const bigIn = 123456789123456789n;
+// const person = {
+//   fName: "Igor",
+//   lName: "Dumanschii",
+//   age: 34,
+//   height: 176,
+// };
+
+// const userName1 = 'Vasile';
+// const userAge1 = 20;
+
+// console.log(typeof userName);
+// console.log(typeof userAge);
+// console.log(typeof learnJS);
+// console.log(typeof jackpot);
+// console.log(typeof nul);
+// console.log(typeof symb);
+// console.log(typeof person);
+// console.log(typeof bigIn);
+
+
+// console.log( userName);
+// console.log( userAge);
+// console.log( learnJS);
+// console.log( jackpot);
+// console.log( nul);
+// console.log( symb);
+// console.log( person);
+// console.log( bigIn);
+
+// console.log(userAge1 - userAge)
+
+// console.log(userAge1 + userAge)
+
+// console.log(userAge1 * userAge)	
+
+// console.log(userAge1 / userAge)
+
+// let age = 20;
+// age++;
+// let age1 =age
+
+// console.log(age1)
+
+
+// let yearBirth = 1990;
+// yearBirth--;
+
+// let yearBirth1 = yearBirth
+
+// console.log(yearBirth1)
+
+// let price1 = 50;
+// console.log(price1 ** 3);
+
+// const ion = 35;
+// const iurii = 35;
+// const maria = 40;
+
+// console.log(ion > maria)
+// console.log(iurii < maria)
+// console.log(ion >= iurii)
+// console.log(ion <= maria)
+
+// const num = 10;
+// const street = '10'
+
+// console.log(num == street)
+// console.log(num === street)
+
+// const adevar = true
+// const minciuna = false
+
+// console.log(!adevar)
+// console.log(!minciuna)
+
+// console.log(adevar != minciuna)
+// console.log(!adevar != minciuna)
